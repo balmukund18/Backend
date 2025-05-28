22e31cc9cb1f9d01976076ac719ca902509c2e21
Balmukund Mishra <sm1370530@gmail.com>
2025-05-29 01:36:19 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -10,10 +10,19 @@ app.use(cors({
 }))
  
 app.use(express.json({limit: "16kb"}));
-app,use(express.urlencoded({extended: true, limit: "16kb"}));
+app.use(express.urlencoded({extended: true, limit: "16kb"}));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+// Importing routes
+import userRoutes from "./routes/user.routes.js";
+
+//routes declaration
+app.use("/api/v1/users", userRoutes);
+
+
+
+export {app};
 
 
 
@@ -22,6 +31,7 @@ app.use(cookieParser());
 
 
 
-export {app}
+
+
 
 
